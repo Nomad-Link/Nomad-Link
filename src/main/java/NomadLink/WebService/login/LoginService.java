be0cf4742e5f44c3b56b54cc94cb0c9c1c89b909
rngustj9139 <mnb9139@khu.ac.kr>
2022-08-31 16:00:23 +0900

    로그인 validation(유효성 체크) 구현

--- src/main/java/NomadLink/WebService/login/LoginService.java
+++ src/main/java/NomadLink/WebService/login/LoginService.java
@@ -6,18 +6,43 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class LoginService {
 
     private final MemberRepository memberRepository;
+    private final EntityManager em;
 
     public Member login(String loginId, String password) {
 
         return memberRepository.findByLoginId(loginId)
                 .filter(m -> m.getPassword().equals(password))
                 .orElse(null);
+    }
+
+    public Optional<Member> findByLoginId(String loginId) {
+
+        return findAll().stream()
+                .filter(m -> m.getUserId().equals(loginId))
+                .findFirst();
+    }
+
+    public Optional<Member> findByPassword(String password) {
+
+        return findAll().stream()
+                .filter(m -> m.getPassword().equals(password))
+                .findFirst();
+    }
+
+    public List<Member> findAll() {
+
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
     }
 
 }
