4d99fae35f136c2d08d38fd397999dfcc190f4dc
rngustj9139 <mnb9139@khu.ac.kr>
2022-09-07 10:38:08 +0900

    마이페이지 기능 구현(지원한 공고 확인 가능)

--- src/main/java/NomadLink/WebService/api/member/MemberMypage.java
+++ src/main/java/NomadLink/WebService/api/member/MemberMypage.java
@@ -5,14 +5,19 @@ import NomadLink.WebService.domain.enterprise.Notice;
 import NomadLink.WebService.domain.member.*;
 import NomadLink.WebService.repository.member.ApplicationRepository;
 import NomadLink.WebService.repository.member.MemberRepository;
+import NomadLink.WebService.session.SessionConst;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,30 +25,32 @@ public class MemberMypage {
 
     private final ApplicationRepository applicationRepository;
 
+    @ResponseBody
     @GetMapping("/api/member/mypage")
-    public void memberMypage(@SessionAttribute Long memberId) {
-        List<Application> applications = applicationRepository.findApplicationsByMember(memberId);
+    public List<MemberMypageResponseDto> memberMypage(@SessionAttribute(name = SessionConst.LOGIN_MEMBER) Member loginMember) {
+        List<Application> applications = applicationRepository.findApplicationsByMember(loginMember.getId());
+
+        List<MemberMypageResponseDto> results = applications.stream()
+                .map(application -> new MemberMypageResponseDto(application))
+                .collect(Collectors.toList());
+
+        return results;
     }
 
     @Data
     static class MemberMypageResponseDto {
 
-        private String realName;
-        private List<MemberMypageNoticesResponseDto> notices;
-
-        public MemberMypageResponseDto() {
-
-        }
-
-    }
-
-    @Data
-    static class MemberMypageNoticesResponseDto {
-
-        private Long noticeId;
         private String enterpriseName;
-        private String companyIntroduction;
+        private String enterpriseLocation;
         private String noticeTitle;
+        private Long noticeId;
+
+        public MemberMypageResponseDto(Application application) {
+            enterpriseName = application.getEnterpriseName();
+            enterpriseLocation = application.getEnterpriseLocation();
+            noticeTitle = application.getNoticeTitle();
+            noticeId = application.getNotice().getId();
+        }
 
     }
 
